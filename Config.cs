5e2e54888f2682be935ce714fffc7ee072ea6912
freeopensourcecode <79154062+freeopensourcecode@users.noreply.github.com>
2021-02-16 16:07:34 +0100

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace OpenCryptShot
+{
+  public class Config
+  {
+    public string apiKey;
+    public string apiSecret;
+    public Decimal quantity;
+    public Decimal sellStrategy;
+    public Decimal strategyrisk;
+    public Decimal maxsecondsbeforesell;
+    public string name;
+    public string expiry;
+    public string discord_token;
+    public string channel_id;
+  }
+}
